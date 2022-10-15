36e14c17a4d1b85e33b5a2d0f8f9071319d03e75
nitish9413 <nitishkatkade94@gmail.com>
2022-10-15 21:47:46 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "deepClassifier"
+AUTHOR_USERNAME = "nitish9413"
+SRC_REPO = "deepClassifier"
+AUTHOR_EMAIL = "nitishkatkade94@gmail.com"
+
+setuptools.setup(
+    name=f"{SRC_REPO}",
+    version=__version__,
+    author=f"{AUTHOR_USERNAME}",
+    author_email=f"{AUTHOR_EMAIL}",
+    description="A package for training deep learning models for image classification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}/issues",
+    }
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
